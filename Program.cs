de6f78c544236d37df8bda8b2cfd029799111727
user <user@BOSTI>
2024-06-07 12:34:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WidgetCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of widgets ordered: ");
+            int numWidgets = Convert.ToInt32(Console.ReadLine());
+
+            double cost;
+            if (numWidgets < 100)
+            {
+                cost = numWidgets * 0.25;
+            }
+            else
+            {
+                cost = numWidgets * 0.20;
+            }
+
+            Console.WriteLine($"The total cost is: {cost:C}");
+        }
+    }
+}
+
